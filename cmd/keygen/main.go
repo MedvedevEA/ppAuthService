430f5fcf9d4889660a97dc83d8f601d49084a373
iMedvedevEA <iMedvedevEA@gmail.com>
2025-05-21 19:08:07 +0700

    feature-logger-fix-keygen-feature-cert

--- cmd/keygen/main.go
+++ cmd/keygen/main.go
@@ -25,12 +25,12 @@ func main() {
 	// Generate RSA keys
 	privateKey, err := rsa.GenerateKey(rand.Reader, keySize)
 	if err != nil {
-		log.Fatalf("Failed to generate private key: %v", err)
+		log.Fatalf("failed to generate private key: %v", err)
 	}
 	// Save the private key to a file
 	privateKeyFile, err := os.Create(privateKeyFileName)
 	if err != nil {
-		log.Fatalf("Failed to create private key file: %v", err)
+		log.Fatalf("failed to create private key file: %v", err)
 	}
 	defer privateKeyFile.Close()
 
@@ -39,25 +39,26 @@ func main() {
 		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
 	}
 	if err := pem.Encode(privateKeyFile, privateKeyPem); err != nil {
-		log.Fatalf("Failed to write private key: %v", err)
+		log.Fatalf("failed to write private key: %v", err)
 	}
 	// Extract the public key and save to a file
 	publicKeyFile, err := os.Create(publicKeyFileName)
 	if err != nil {
-		log.Fatalf("Failed to create public key file: %v", err)
+		log.Fatalf("failed to create public key file: %v", err)
 	}
 	defer publicKeyFile.Close()
 
 	publicKeyBytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
 	if err != nil {
-		log.Fatalf("Failed to marshal public key: %v", err)
+		log.Fatalf("failed to marshal public key: %v", err)
 	}
 	publicKeyPem := &pem.Block{
 		Type:  "RSA PUBLIC KEY",
 		Bytes: publicKeyBytes,
 	}
 	if err := pem.Encode(publicKeyFile, publicKeyPem); err != nil {
-		log.Fatalf("Failed to write public key: %v", err)
+		log.Fatalf("failed to write public key: %v", err)
 	}
+	log.Println("key creation completed successfully")
 
 }
